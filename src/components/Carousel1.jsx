c82513f208a8633582d0a587f337418c972ea463
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-31 21:16:27 +0545

    carousel modified

--- src/components/Carousel1.jsx
+++ src/components/Carousel1.jsx
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel'
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
+import './Carousel.css'; // Custom CSS file for styling
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import image1 from '../assets/nepal1.jpg'
 import image2 from '../assets/nepal2.jpg'
 import image3 from '../assets/nepal3.jpg'
 
+const Carousel = () => {
+  const sliderRef = useRef(null);
 
-class DemoCarousel extends Component {
-    render() {
-        return (
-            <Carousel>
-                <div className='car'>
-                    <img src={image1} />
-                    <p className='car-text'>Nepal is beautifull</p>
-                </div>
-                <div>
-                    <img src={image2} />
-                    <p className='car-text'>Nepal is beautifull</p>
-                </div>
-                <div>
-                    <img src={image3} />
-                    <p className='car-text'>Nepal is beautifull</p>
-                </div>
-            </Carousel>
-        );
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="carousel-container">
+      <Slider ref={sliderRef} {...settings}>
+        <div className="carousel-slide">
+          <img src={image1}alt="First slide" />
+        </div>
+        <div className="carousel-slide">
+          <img src={image2} alt="Second slide" />
+        </div>
+        <div className="carousel-slide">
+          <img src={image3} alt="Third slide" />
+        </div>
+      </Slider>
+
+      {/* Custom Prev/Next Buttons inside the image */}
+      <button className="prev-button" onClick={() => sliderRef.current.slickPrev()}>
+        &#10094;
+      </button>
+      <button className="next-button" onClick={() => sliderRef.current.slickNext()}>
+        &#10095;
+      </button>
+    </div>
+  );
 };
 
-export default DemoCarousel;
+export default Carousel;
