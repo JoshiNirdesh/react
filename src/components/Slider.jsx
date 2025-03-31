d08be3635fc8ce88b599fdfa8a322454d9fd8902
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-31 21:26:09 +0545

    slidder update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,21 +1,67 @@
 import React from 'react';
-import ReactCardSlider from 'react-card-slider-component';
+import Slider from 'react-slick';
 
-const Slider = () => {
-  
-  const sliderClick = (slide) => {
-    console.log("Slide clicked:", slide);
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import image1 from '../assets/nepal1.jpg'
+import image2 from '../assets/nepal2.jpg'
+import image3 from '../assets/nepal3.jpg'
+
+// Example card component
+const CardSlider = () => {
+  const settings = {
+    dots: true, // Show dots for navigation
+    infinite: true, // Infinite loop
+    speed: 500, // Transition speed
+    slidesToShow: 3, // Number of cards to show at once
+    slidesToScroll: 1, // Number of cards to scroll at once
+    nextArrow: <button className="slick-next">Next</button>, // Custom Next button
+    prevArrow: <button className="slick-prev">Prev</button>, // Custom Prev button
+    responsive: [
+      {
+        breakpoint: 1024, // For devices larger than 1024px
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768, // For devices larger than 768px
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   };
 
-  const slides = [
-    { image: "https://picsum.photos/200/300", title: "Title 1", description: "Description 1", clickEvent: () => sliderClick("Title 1") },
-    { image: "https://picsum.photos/600/500", title: "Title 2", description: "Description 2", clickEvent: () => sliderClick("Title 2") },
-    { image: "https://picsum.photos/700/600", title: "Title 3", description: "Description 3", clickEvent: () => sliderClick("Title 3") },
-    { image: "https://picsum.photos/500/400", title: "Title 4", description: "Description 4", clickEvent: () => sliderClick("Title 4") },
-    { image: "https://picsum.photos/800/700", title: "Title 5", description: "Description 5", clickEvent: () => sliderClick("Title 5") },
-  ];
-
-  return <ReactCardSlider slides={slides} />;
+  return (
+    
+    <div className="slider-container">
+      <h1 className='about-title'>About Us</h1>
+      <Slider {...settings}>
+        <div className="card">
+          <img src={image1} alt="Card 1" />
+          <h3>Card 1</h3>
+          <p>Some content about Card 1</p>
+        </div>
+        <div className="card">
+          <img src={image2} alt="Card 2" />
+          <h3>Card 2</h3>
+          <p>Some content about Card 2</p>
+        </div>
+        <div className="card">
+          <img src={image3} alt="Card 3" />
+          <h3>Card 3</h3>
+          <p>Some content about Card 3</p>
+        </div>
+        <div className="card">
+          <img src={image1} alt="Card 4" />
+          <h3>Card 4</h3>
+          <p>Some content about Card 4</p>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
-export default Slider;
+export default CardSlider;
